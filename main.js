aeffac2fb07e72d25e13d313e51fd6ce0c1a9765
alanaamerico22 <145569370+alanaamerico22@users.noreply.github.com>
2025-06-17 08:04:02 -0300

    sss

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-05T14:00:00");
-const tempoObjetivo2 = new Date("2025-06-26T15:00:00");
-const tempoObjetivo3 = new Date("2025-12-20T20:00:00");
-const tempoObjetivo4 = new Date("2025-11-30T14:00:00");
+const tempoObjetivo1 = new Date("2025-11-09T14:00:00");
+const tempoObjetivo2 = new Date("2025-10-16T15:00:00");
+const tempoObjetivo3 = new Date("2025-01-10T20:00:00");
+const tempoObjetivo4 = new Date("2025-20-10T14:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
