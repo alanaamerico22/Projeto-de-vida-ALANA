3cbbf84606aab985854e5b84262e40f6df339fc0
alanaamerico22 <145569370+alanaamerico22@users.noreply.github.com>
2025-07-01 07:38:38 -0300

    hhh

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-09T14:00:00");
 const tempoObjetivo2 = new Date("2025-10-16T15:00:00");
-const tempoObjetivo3 = new Date("2025-01-10T20:00:00");
-const tempoObjetivo4 = new Date("2025-20-10T14:00:00");
+const tempoObjetivo3 = new Date("2026-01-10T20:00:00");
+const tempoObjetivo4 = new Date("2026-20-10T14:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
